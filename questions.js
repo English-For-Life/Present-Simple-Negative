738d9d763e1ee915c37a0edf2a25b98630b4ffd7
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:15:30 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"He ___ live in London.",
+        options:["do not", "does not", "is not", "don't"],
+        answer: 1
+    },
+    {
+     q:"I ___ get up at 7 o'clock every day.",
+        options:["don't", "does not", "am not", "doesn't"],
+        answer: 0   
+    },
+    {
+        q:"She ___ work ten hours a day.",
+        options:["do not", "isn't", "doesn't", "don't"],
+        answer: 2
+    },
+    {
+        q:"They ___ go to school.",
+        options:["doesn't likes", "doesn't like", "don't likes", "don't like"],
+        answer: 3
+    },
+    {
+        q:"We ___ to bed at 9 o'clock.",
+        options:["doesn't go", "are not go", "don't go", "don't goes"],
+        answer: 2
+    },
+    {
+        q:"He ___ on Saturdays.",
+        options:["don't work", "does not work", "doesn't works", "isn't work"],
+        answer: 1
+    },
+    {
+        q:"I ___ get up early.",
+        options:["do not like", "don't likes", "am not like", "doesn't like"],
+        answer: 0
+    },
+    {
+        q:"My mum ___ TV in the morning.",
+        options:["isn't watch", "don't watch", "doesn't watches", "doesn't watch"],
+        answer: 3
+    },
+    {
+        q:"My friend often ___ homework.",
+        options:["don't does", "doesn't do", "don't do", "isn't do"],
+        answer: 1
+    },
+    {
+        q:"It ___ very often here.",
+        options:["don't rains", "doesn't rains", "doesn't rain", "don't rain"],
+        answer: 2
+    }
+
+]
